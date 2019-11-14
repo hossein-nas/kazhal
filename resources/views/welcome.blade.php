dbacadefa8b10ef137c7d976a141e326e883ab7c
hossein-nas <hossein.nasiri.sovari@gmail.com>
2019-11-15 00:13:06 +0330

    working on home page header area  - changing header.svg from img tag to background-image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,17 +7,19 @@
 
         <title>رایان پردازش کژال</title>
         <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
-	<link rel="manifest" href="./favicon/site.webmanifest">
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+        <link rel="manifest" href="./favicon/site.webmanifest">
     
 
     </head>
     <body>
         <header class="header">
-            <img src="./img/svg/header.svg" alt="header" class="head-shape">
-            <div class="roban">
-                <img src="./img/svg/logo.social.svg" alt="" class="logo">
+            <!-- <img src="./img/svg/header.svg" alt="header" class="head-shape"> -->
+            <div class="container">
+                <div class="roban">
+                    <img src="./img/svg/logo.social.svg" alt="" class="logo">
+                </div>
             </div>
 
         </header>
