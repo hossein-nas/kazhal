2740e551bf586d80bb958403367dc313e04caf10
hossein-nas <hossein.nasiri.sovari@gmail.com>
2019-11-16 16:03:04 +0330

    adding "Iran Yekan Font" and working on responsive header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,20 @@
                 <div class="roban">
                     <img src="./img/svg/logo.social.svg" alt="" class="logo">
                 </div>
+                <div class="slog">
+                    <div class="main-picture">
+
+                    </div>
+                    <p class="first">به دنیای فناوری سلام کنید.</p>
+                    <p class="second">
+                        دیجیتــــــالی شــــــــدن کســـــــــب‌و‌کـــــــــار
+                        <br/>
+                        خود را به ما بسپارید
+                    </p>
+                    <p class="bold">
+                        تــا مـــوفقیت همـــراهتان هستیــم
+                    </p>
+                </div>
             </div>
 
         </header>
