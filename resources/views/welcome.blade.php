9acd8c39aa31a892ee2fa24075216ce3d8544b08
hossein-nas <hossein.nasiri.sovari@gmail.com>
2019-11-23 10:53:44 +0330

    Wokring on 'first page' styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,8 @@
     </head>
     <body>
         <header class="header">
-            <!-- <img src="./img/svg/header.svg" alt="header" class="head-shape"> -->
+            <div class="hamberger"></div>
+            <div class="search"></div>
             <div class="container">
                 <div class="roban">
                     <img src="./img/svg/logo.social.svg" alt="" class="logo">
@@ -37,9 +38,229 @@
             </div>
 
         </header>
-        <section>
-            salam
+        <section class="services" id="services">
+
+            <div class="service website">
+                <div class="icon">
+
+                </div>
+                <div class="header">طراحی وب‌سایت</div>
+                <div class="desc">
+                تیمی متشکل از بهترین‌های این حوزه آماده هستند بهترین راه‌کارها را برای دیجیتالی‌شدن کسب‌وکارتان در جهت …
+                </div>
+            </div>
+
+            <div class="service desktop-app">
+                <div class="icon">
+
+                </div>
+                <div class="header">اپلیکیشن دسکتاپ</div>
+                <div class="desc">
+                    انتخابی عالی برای کسانی که می‌خواهند یک قدم از رقبای خود پیش بیفتند و خدمات بهتری به مشتریان خود ارائه دهند ...
+                </div>
+            </div>
+
+            <div class="service android-app">
+                <div class="icon">
+
+                </div>
+                <div class="header">اپلیکیشن اندروید</div>
+                <div class="desc">
+                    انتخابی عالی برای کسانی که می‌خواهند یک قدم از رقبای خود پیش بیفتند و خدمات بهتری به مشتریان خود ارائه دهند ...
+                </div>
+            </div>
+
+            <div class="service telegram-robot">
+                <div class="icon">
+
+                </div>
+                <div class="header">ربات تلگرام</div>
+                <div class="desc">
+                    انتخابی عالی برای کسانی که می‌خواهند یک قدم از رقبای خود پیش بیفتند و خدمات بهتری به مشتریان خود ارائه دهند ...
+                </div>
+            </div>
+
+
+            <div class="service seo">
+                <div class="icon">
+
+                </div>
+                <div class="header">بهینه‌سازی سئو</div>
+                <div class="desc">
+                    انتخابی عالی برای کسانی که می‌خواهند یک قدم از رقبای خود پیش بیفتند و خدمات بهتری به مشتریان خود ارائه دهند ...
+                </div>
+            </div>
+
         </section>
+
+        <section id="pricing">
+            <div class="panel panel-1">
+                <div class="header">
+                    طراحی وب‌سایت
+                </div>
+                <div class="price">
+                    ۸۰۰٫۰۰۰
+                </div>
+                <div class="desc">
+                    تیمی متشکل از بهترین‌های این حوزه آماده هستند بهترین راه‌کارها را برای دیجیتالی‌شدن کسب‌وکارتان در جهت …
+                </div>
+                <div class="order">
+                    جزئیات بیشتر و مشاوره
+                </div>
+
+            </div>
+
+            <div class="panel panel-2">
+                <div class="header">
+                   ربات تلگرام 
+                </div>
+                <div class="price">
+                    ۸۰۰٫۰۰۰
+                </div>
+                <div class="desc">
+                    تیمی متشکل از بهترین‌های این حوزه آماده هستند بهترین راه‌کارها را برای دیجیتالی‌شدن کسب‌وکارتان در جهت …
+                </div>
+                <div class="order">
+                    جزئیات بیشتر و مشاوره
+                </div>
+
+            </div>
+
+            <div class="panel panel-3">
+                <div class="header">
+                    سئــــو
+                    SEO
+                </div>
+                <div class="price">
+                    ۸۰۰٫۰۰۰
+                </div>
+                <div class="desc">
+                    تیمی متشکل از بهترین‌های این حوزه آماده هستند بهترین راه‌کارها را برای دیجیتالی‌شدن کسب‌وکارتان در جهت …
+                </div>
+                <div class="order">
+                    جزئیات بیشتر و مشاوره
+                </div>
+
+            </div>
+        </section>
+
+        <section id="news">
+            <div class="item">
+                <a href="#">
+                    <img src="../img/1.jpg" alt="news">
+                    <p>
+                        تخفیف به مناسبت نیمه شعبان
+                    </p>
+                </a>
+            </div>
+
+            <div class="item">
+                <a href="#">
+                    <img src="../img/1.jpg" alt="news">
+                    <p>
+                        تخفیف به مناسبت نیمه شعبان
+                    </p>
+                </a>
+            </div>
+
+            <div class="item">
+                <a href="#">
+                    <img src="../img/1.jpg" alt="news">
+                    <p>
+                        تخفیف به مناسبت نیمه شعبان
+                    </p>
+                </a>
+            </div>
+
+            <div class="more-news">
+                <a href="#">
+                    مشاهده بیشتر
+                </a>
+            </div>
+        </section>
+
+        <footer>
+            <section class="social-links">
+                <ul class="social-icons">
+                    <li class="icon">
+                        <a href="#"> <img src="../img/svg/social/insta.svg" alt=""></a>
+                    </li>
+                    <li class="icon">
+                        <a href="#"> <img src="../img/svg/social/telegram.svg" alt=""></a>
+                    </li>
+                    <li class="icon">
+                        <a href="#"> <img src="../img/svg/social/skype.svg" alt=""></a>
+                    </li>
+                    <li class="icon">
+                        <a href="#"> <img src="../img/svg/social/youtube.svg" alt=""></a>
+                    </li>
+                    <li class="icon">
+                        <a href="#"> <img src="../img/svg/social/aparat.svg" alt=""></a>
+                    </li>
+                </ul>
+            </section>
+
+            <section class="footer-links">
+                <ul class="links">
+                    <li><a href="#">صفحه اصلی</a></li>
+                    <li><a href="#">درباره‌ی ما</a></li>
+                    <li><a href="#">خدمات</a></li>
+                    <li><a href="#">تماس با ما</a></li>
+                    <li><a href="#">تعرفه‌ها</a></li>
+                    <li><a href="#">نقشه‌ی سایت</a></li>
+                    <li><a href="#">مقالات</a></li>
+                </ul>
+            </section>
+
+            <section class="contact">
+                <div class="address">
+                    <div class="icon"></div>
+                    <div class="address-text">
+                        تهران 
+                        <span>/</span>
+                        شهر قدس 
+                        <span>/</span>
+                        خیابان جمهوری 
+                        <br/>
+                        مجتمع ابریشم 
+                        <span>/</span>
+                        طبقه‌ی اول
+                        <span>/</span>
+                        واحد ۱۰۱ 
+                    </div>
+                </div>
+
+                <div class="phone-numbers">
+                    <div class="icon">
+
+                    </div>
+
+                    <div class="numbers">
+                        <div>۸۴۵۵</div>
+                        <div>۶۴۸</div>
+                        <div>۰۹۳۶</div>
+                        <div>۸۵۱۳</div>
+                        <div>۶۶۳</div>
+                        <div>۰۹۰۵</div>
+                    </div>
+                </div>
+
+            </section>
+
+            <section class="map">
+
+            </section>
+
+            <section class="copyright">
+                <span>تمامی حقوق مادی و معنوی این سایت متعلق به</span>
+                <span>
+                    شرکت 
+                    <img src="../img/svg/rpkazhal.svg" alt="رایان پردازش کژال">
+                    می‌باشد.
+                </span>
+            </section>
+
+        </footer>
 
     <script src="{{ mix('/js/app.js') }}"></script>
     </body>
