c16d98a1d0cd106084eb75efde3e71f3a9bb33bb
hossein-nas <hossein.nasiri.sovari@gmail.com>
2019-11-25 12:58:51 +0000

    managing blade layouts and working on navbar

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,84 @@
+<footer>
+    <div class="container">
+        <section class="social-links">
+            <ul class="social-icons">
+                <li class="icon">
+                    <a href="#"> <img src="../img/svg/social/insta.svg" alt=""></a>
+                </li>
+                <li class="icon">
+                    <a href="#"> <img src="../img/svg/social/telegram.svg" alt=""></a>
+                </li>
+                <li class="icon">
+                    <a href="#"> <img src="../img/svg/social/skype.svg" alt=""></a>
+                </li>
+                <li class="icon">
+                    <a href="#"> <img src="../img/svg/social/youtube.svg" alt=""></a>
+                </li>
+                <li class="icon">
+                    <a href="#"> <img src="../img/svg/social/aparat.svg" alt=""></a>
+                </li>
+            </ul>
+        </section>
+
+        <section class="footer-links">
+            <ul class="links">
+                <li><a href="#">صفحه اصلی</a></li>
+                <li><a href="#">درباره‌ی ما</a></li>
+                <li><a href="#">خدمات</a></li>
+                <li><a href="#">تماس با ما</a></li>
+                <li><a href="#">تعرفه‌ها</a></li>
+                <li><a href="#">نقشه‌ی سایت</a></li>
+                <li><a href="#">مقالات</a></li>
+            </ul>
+        </section>
+
+        <section class="contact">
+            <div class="address">
+                <div class="icon"></div>
+                <div class="address-text">
+                    تهران
+                    <span>/</span>
+                    شهر قدس
+                    <span>/</span>
+                    خیابان جمهوری
+                    <br />
+                    مجتمع ابریشم
+                    <span>/</span>
+                    طبقه‌ی اول
+                    <span>/</span>
+                    واحد ۱۰۱
+                </div>
+            </div>
+
+            <div class="phone-numbers">
+                <div class="icon">
+
+                </div>
+
+                <div class="numbers">
+                    <div>۸۴۵۵</div>
+                    <div>۶۴۸</div>
+                    <div>۰۹۳۶</div>
+                    <div>۸۵۱۳</div>
+                    <div>۶۶۳</div>
+                    <div>۰۹۰۵</div>
+                </div>
+            </div>
+
+            <div class="map">
+            </div>
+
+        </section>
+
+
+        <section class="copyright">
+            <span>تمامی حقوق مادی و معنوی این سایت متعلق به</span>
+            <span>
+                شرکت
+                <img src="../img/svg/rpkazhal.svg" alt="رایان پردازش کژال">
+                می‌باشد.
+            </span>
+        </section>
+    </div>
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header class="header">
+    <div class="hamberger"></div>
+    <div class="search"></div>
+    <div class="container">
+        <div class="roban">
+            <img src="./img/svg/logo.social.svg" alt="" class="logo">
+        </div>
+        <nav>
+            <ul class="menubar">
+                <li class="item"><a href="#">خانه</a></li>
+                <li class="item"><a href="#">خدمات</a></li>
+                <li class="item"><a href="#">تعرفه‌ها</a></li>
+                <li class="item"><a href="#">دربـاره‌ی مـا</a></li>
+                <li class="item"><a href="#">تماس با مـا</a></li>
+            </ul>
+        </nav>
+        <div class="slog">
+            <div class="main-picture">
+
+            </div>
+            <p class="first">به دنیای فناوری سلام کنید.</p>
+            <p class="second">
+                دیجیتــــــالی شــــــــدن کســـــــــب‌و‌کـــــــــار
+                <br />
+                خود را به ما بسپارید
+            </p>
+            <p class="bold">
+                تــا مـــوفقیت همـــراهتان هستیــم
+            </p>
+        </div>
+    </div>
+
+</header>
