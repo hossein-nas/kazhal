6728bc1762bb2d98e5304644fda5d03e6106817e
hossein-nas <hossein.nasiri.sovari@gmail.com>
2019-12-01 10:19:25 +0000

    fixing some inssues and working on navbar sidebar on mobile

--- public/js/app.js
+++ public/js/app.js
@@ -36982,35 +36982,49 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 $(document).ready(function () {
   manageNavbarAnchorLinks();
+  managingGettingStarted();
+  managingHashChange(); // this is triggered when page first load along with hash
+
   $(window).bind('hashchange', function (e) {
     e.preventDefault();
-    var hash = window.location.hash;
-
-    if (hash == "#home") {
-      $('body, html').animate({
-        'scrollTop': 0
-      }, 300);
-    }
-
-    if (hash == "#services-section") {
-      var services_top = $('#services').offset().top;
-      $('body, html').animate({
-        'scrollTop': services_top - 30
-      }, 300);
-    }
-
-    if (hash == "#pricing-section") {
-      var pricing_top = $('#pricing').offset().top;
-      $('body, html').animate({
-        'scrollTop': pricing_top - 70
-      }, 300);
-    }
+    managingHashChange();
   });
 });
+
+function managingHashChange() {
+  var hash = window.location.hash;
+
+  if (hash == "#home") {
+    $('body, html').animate({
+      'scrollTop': 0
+    }, 300);
+  }
+
+  if (hash == "#services-section") {
+    var services_top = $('#services').offset().top;
+    $('body, html').animate({
+      'scrollTop': services_top - 30
+    }, 300);
+  }
+
+  if (hash == "#pricing-section") {
+    var pricing_top = $('#pricing').offset().top;
+    $('body, html').animate({
+      'scrollTop': pricing_top - 70
+    }, 300);
+  }
+}
 
 function manageNavbarAnchorLinks() {
   $('nav a').click(function () {
     window.location.hash = "#";
+  });
+}
+
+function managingGettingStarted() {
+  $('header p.bold').click(function () {
+    window.location.hash = '#';
+    window.location.hash = '#services-section';
   });
 }
 
@@ -37027,6 +37041,7 @@ var sidebar_status = 0;
 var stickynavbar_status = 0;
 var search_status = 0;
 $(document).ready(function () {
+  headerButtonEvents();
   $(document).scroll(function () {
     var _header = $('.header');
 
@@ -37049,14 +37064,49 @@ $(document).ready(function () {
   });
 });
 
+function headerButtonEvents() {
+  $('.hamberger').click(function () {
+    alert('hi');
+
+    var _nav = $('nav');
+
+    if (_nav.hasClass('active')) {
+      navbarFadOut();
+    } else {
+      navbarFadeIn();
+    }
+  });
+}
+
 function hamburgerClickEvent() {
   $('.hamburger').click(function () {
-    $('nav').addClass('active');
-    sidebar_status = 1;
-    setTimeout(function () {
-      $('nav ul').addClass('show');
-    }, 100);
+    var _nav = $('nav');
+
+    if (_nav.hasClass('active')) {
+      navbarFadOut();
+    } else {
+      navbarFadeIn();
+    }
   });
+  $('nav').click(function () {
+    navbarFadOut();
+  });
+}
+
+function navbarFadOut() {
+  $('nav ul').removeClass('show');
+  sidebar_status = 0;
+  setTimeout(function () {
+    $('nav').removeClass('active');
+  }, 200);
+}
+
+function navbarFadeIn() {
+  $('nav').addClass('active');
+  sidebar_status = 1;
+  setTimeout(function () {
+    $('nav ul').addClass('show');
+  }, 100);
 }
 
 function fixedmenuHTML() {
