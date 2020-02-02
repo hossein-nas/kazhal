2f5d7e8da20cb22193af8c39c1be902ad215d0a3
hossein-nas <hossein.nasiri.sovari@gmail.com>
2020-02-03 00:03:15 +0330

    fixing production and devlopment build issue with quasar

--- q-app/src/boot/axios.js
+++ q-app/src/boot/axios.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import axios from 'axios'
+var baseURL = 'http://kazhal.test/'
+
+if (process.env.PROD) {
+    baseURL = 'https://rp-kazhal.rp/'
+}
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Accept'] = 'application/json'
 
 let _axios = axios.create({
-    baseURL: 'http://kazhal.test/'
+    baseURL: baseURL
 })
 Vue.prototype.$axios = _axios
 
